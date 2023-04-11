881ed1c7e556ca5cc980f3de67d65b61f9f0d76d
Tim Beadle <tim@takkconsulting.co.uk>
2023-04-11 14:14:30 +0100

    xo -> eslint; fix eslint issues

--- tasks/tv4.js
+++ tasks/tv4.js
@@ -16,7 +16,6 @@ const loader = require('../lib/loader');
 const runnerModule = require('../lib/runner');
 
 module.exports = (grunt) => {
-
 	const out = miniwrite.grunt(grunt);
 	const style = ministyle.grunt();
 	const report = reporter.getReporter(out, style);
@@ -24,15 +23,15 @@ module.exports = (grunt) => {
 		taskTv4,
 		loader.getLoaders(),
 		out,
-		style
+		style,
 	);
 
-	grunt.registerMultiTask('tv4', 'Validate values against json-schema v4.', function() {
+	grunt.registerMultiTask('tv4', 'Validate values against json-schema v4.', function () {
 		const done = this.async();
 
 		// Import options
 		const context = runner.getContext(this.options(runner.getOptions({
-			timeout: 5000
+			timeout: 5000,
 		})));
 
 		const objects = [];
@@ -43,13 +42,13 @@ module.exports = (grunt) => {
 			context.tv4 = taskTv4;
 		}
 
-		grunt.util._.each(context.options.schemas, function (schema, uri) {
+		grunt.util._.each(context.options.schemas, (schema, uri) => {
 			context.tv4.addSchema(uri, schema);
 		});
 
 		context.tv4.addFormat(context.options.formats);
 
-		grunt.util._.each(context.options.languages, function (language, id) {
+		grunt.util._.each(context.options.languages, (language, id) => {
 			context.tv4.addLanguage(id, language);
 		});
 
@@ -58,8 +57,8 @@ module.exports = (grunt) => {
 		}
 
 		// Flatten list for sanity
-		grunt.util._.each(this.files, function (f) {
-			grunt.util._.some(f.src, function (filePath) { // eslint-disable-line unicorn/no-fn-reference-in-iterator
+		grunt.util._.each(this.files, (f) => {
+			grunt.util._.some(f.src, (filePath) => {
 				if (!grunt.file.exists(filePath)) {
 					grunt.log.warn('file "' + filePath + '" not found.');
 					return true;
@@ -68,7 +67,7 @@ module.exports = (grunt) => {
 				objects.push({
 					path: filePath,
 					label: filePath,
-					root: context.options.root
+					root: context.options.root,
 				});
 			});
 		});
@@ -82,7 +81,7 @@ module.exports = (grunt) => {
 
 		if (typeof values === 'object') {
 			const keyPrefix = (Array.isArray(values) ? 'value #' : '');
-			grunt.util._.some(values, function (value, key) { // eslint-disable-line unicorn/no-fn-reference-in-iterator
+			grunt.util._.some(values, (value, key) => {
 				objects.push({
 					label: keyPrefix + key,
 					value,
@@ -93,7 +92,7 @@ module.exports = (grunt) => {
 		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 		if (context.options.add && Array.isArray(context.options.add)) {
-			grunt.util._.some(context.options.add, function (schema) { // eslint-disable-line unicorn/no-fn-reference-in-iterator
+			grunt.util._.some(context.options.add, (schema) => {
 				if (typeof schema === 'string') {
 					// Juggle
 					schema = grunt.file.readJSON(schema);
@@ -115,7 +114,7 @@ module.exports = (grunt) => {
 
 		// grunt.verbose.writeln(util.inspect(context));
 
-		context.validate(objects, function (err, job) {
+		context.validate(objects, (err, job) => {
 			if (err) {
 				throw err;
 			}
